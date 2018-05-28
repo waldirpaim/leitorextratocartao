0dc23eba93af78f52a49930c99a7a113841e4d74
waldirpaim <waldirpaim@gmail.com>
2018-05-28 11:30:09 -0400

    Implementa Layout2 da REDE

--- LeitorExtratoCartaoRede.pas
+++ LeitorExtratoCartaoRede.pas
@@ -5,6 +5,7 @@ interface
 uses
   Classes,
   SysUtils,
+  Math,
   LeitorExtratoCartao;
 
 type
@@ -13,6 +14,7 @@ type
   TLeitorExtratoCartaoRede = class(TOperadoraCartao)
   private
     procedure Layout1(ARetorno: TStrings);
+    procedure Layout2(ARetorno: TStrings);
   public
     constructor Create(AOwner: TLeitorExtratoCartao);
     procedure LerExtrato(const ANomeArq: string); override;
@@ -63,6 +65,94 @@ begin
   end;
 end;
 
+procedure TLeitorExtratoCartaoRede.Layout2(ARetorno: TStrings);
+var
+  VTmp: TStringList;
+  VArray: TArray<string>;
+  Vdescricao: string;
+  VTipo : string;
+  VDesconto: Extended;
+  VDifDesconto: Extended;
+  VSomaDesconto: Extended;
+  VValDesc : Extended;
+  VPerDesconto: Extended;
+  VValBruto: Extended;
+  VParcela: TParcelaCartao;
+begin
+  VTmp := TStringList.Create;
+  try
+    VTmp.AddPair('separador', ';');
+    VTmp.AddPair('quote', '"');
+    VTmp.AddPair('linhainicial', '11');
+    VTmp.AddPair('nsudoc', '1');
+    VTmp.AddPair('codautorizacao', '-1');
+    VTmp.AddPair('valorbruto', '6');
+    VTmp.AddPair('descricao', '-1');
+    VTmp.AddPair('valorliquido', '-1');
+    VTmp.AddPair('valordesconto', '-1');
+    VTmp.AddPair('datavenda', '4');
+    VTmp.AddPair('dataprevista', '-1');
+    VTmp.AddPair('tipotransacao', '-1');
+    VTmp.AddPair('numerocartao', '2');
+    VTmp.AddPair('numparcelas', '-1');
+    ProcessaTemplate(ARetorno, VTmp);
+
+    Vdescricao := '';
+
+    VArray := ARetorno[2].Split([';']);
+    if Length(VArray) >= 1 then
+      Vdescricao := ColunaStr([VArray[1]], '0', '');
+
+    VTipo := '';
+
+    VArray := ARetorno[3].Split([';']);
+    if Length(VArray) >= 1 then
+      VTipo := ColunaStr([VArray[1]], '0', '');
+
+    Vdescricao := Concat( Vdescricao, ' ', VTipo );
+
+    VValBruto := 0;
+
+    VArray := ARetorno[5].Split([';']);
+    if Length(VArray) >= 1 then
+      VValBruto := ColunaFloat([VArray[1]], '0', '');
+
+    VDesconto := 0;
+
+    VArray := ARetorno[6].Split([';']);
+    if Length(VArray) >= 1 then
+      VDesconto := ColunaFloat([VArray[1]], '0', '');
+
+    VPerDesconto := 0;
+
+    if VDesconto > 0 then
+      VPerDesconto := (VDesconto / VValBruto * 100);
+
+    VSomaDesconto := 0;
+
+    for VParcela in FListaDeParcelas do
+    begin
+      VParcela.descricao := Vdescricao;
+      VParcela.tipotransacao := VTipo;
+      VValDesc := RoundTo( VParcela.valorbruto * VPerDesconto / 100, -2);
+      VParcela.valordesconto := VValDesc;
+      VSomaDesconto := Sum([VSomaDesconto, VParcela.valordesconto]);
+      VParcela.valorliquido := VParcela.valorbruto - VParcela.valordesconto;
+      if CompareValue(VSomaDesconto, VDesconto, 0.0001) > 0 then
+      begin
+        VDifDesconto := RoundTo(VSomaDesconto - VDesconto, 2);
+        if VDifDesconto > 0 then
+          VParcela.valordesconto := VParcela.valordesconto + VDifDesconto
+        else if VParcela.valordesconto > Abs(VDifDesconto) then
+          VParcela.valordesconto := VParcela.valordesconto - VDifDesconto;
+      end;
+    end;
+
+  finally
+    VTmp.Free;
+  end;
+end;
+
 procedure TLeitorExtratoCartaoRede.LerExtrato(const ANomeArq: string);
 var
   VRetorno: TStringList;
@@ -75,6 +165,8 @@ begin
     case ValidaArquivo(VRetorno) of
       1:
         Layout1(VRetorno);
+      2:
+        Layout2(VRetorno);
     else
       raise Exception.CreateResFmt(@SARQUIVO_FORA_FORMATO, [ANomeArq]);
     end;
@@ -85,9 +177,14 @@ begin
 end;
 
 function TLeitorExtratoCartaoRede.ValidaArquivo(ARetorno: TStrings): Integer;
+var
+  VText: string;
 begin
-  if SameText(Copy(ARetorno[0], 1, 14), 'Extrato Rede -') then
+  VText := Copy(ARetorno[0], 1, 14);
+  if SameText(VText, 'Extrato Rede -') then
     Exit(1);
+  if SameText(VText, 'data da venda;') and SameText(Copy(ARetorno[1], 1, 14), 'resumo vendas;') then
+    Exit(2);
   Result := 0;
 end;
 
